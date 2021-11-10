62c30351fd8a1feb92d05500c9f6d70731e8a26a
Mykhailo Saveliev <Mykhailo_Saveliev@epam.com>
2021-11-10 10:39:55 +0200

    added placeholder for message routing project

---
+++ src/Device.Routers/MessageRouter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MessageRouter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
