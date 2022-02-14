0f7415b79eab70b8a6e1bc367d0637e4c1590b01
Mykhailo Saveliev <mykhailo_saveliev@epam.com>
2022-02-14 15:44:51 +0200

    removed core serializer from middleware as this is asp pipeline responsibility

--- src/Scheduler/Scheduler.Api/Middlewares/ErrorHandlingMiddleware.cs
+++ src/Scheduler/Scheduler.Api/Middlewares/ErrorHandlingMiddleware.cs
@@ -1,62 +1,55 @@
 ﻿using Assistant.Net.Abstractions;
 using Assistant.Net.Scheduler.Contracts.Exceptions;
-using Assistant.Net.Serialization.Abstractions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 
-namespace Assistant.Net.Scheduler.Api.Middlewares
+namespace Assistant.Net.Scheduler.Api.Middlewares;
+
+internal class ErrorHandlingMiddleware : IMiddleware
 {
-    internal class ErrorHandlingMiddleware : IMiddleware
+    private readonly ISystemLifetime lifetime;
+    private readonly ILogger logger;
+
+    public ErrorHandlingMiddleware(
+        ILogger<ErrorHandlingMiddleware> logger,
+        ISystemLifetime lifetime)
     {
-        private readonly ILogger logger;
-        private readonly ISerializer<ProblemDetails> serializer;
-        private readonly ISystemLifetime lifetime;
-
-        public ErrorHandlingMiddleware(
-            ILogger<ErrorHandlingMiddleware> logger,
-            ISerializer<ProblemDetails> serializer,
-            ISystemLifetime lifetime)
-        {
-            this.logger = logger;
-            this.serializer = serializer;
-            this.lifetime = lifetime;
-        }
-
-        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
-        {
-            try
-            {
-                await next(context);
-            }
-            catch (NotFoundException e)
-            {
-                logger.LogDebug(e, "An exception has caught.");
-
-                context.Response.StatusCode = StatusCodes.Status404NotFound;
-                await serializer.Serialize(context.Response.Body, ResourceNotFoundProblem, lifetime.Stopping);
-            }
-            catch (Exception e)
-            {
-                logger.LogError(e, "An exception has caught.");
-
-                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                await serializer.Serialize(context.Response.Body, RequestFailedProblem, lifetime.Stopping);
-            }
-        }
-
-        private static ProblemDetails ResourceNotFoundProblem => new()
-        {
-            Title = "Resource wasn't found.",
-            Detail = "Requested resource or one of its dependencies wasn't found."
-        };
-
-        private static ProblemDetails RequestFailedProblem => new()
-        {
-            Title = "Request has failed.",
-            Detail = "Unexpected internal error has occurred."
-        };
+        this.logger = logger;
+        this.lifetime = lifetime;
     }
+
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (NotFoundException e)
+        {
+            logger.LogDebug(e, "An exception has caught.");
+
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+            await context.Response.WriteAsJsonAsync(ResourceNotFoundProblem, lifetime.Stopping);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "An exception has caught.");
+
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            await context.Response.WriteAsJsonAsync(RequestFailedProblem, lifetime.Stopping);
+        }
+    }
+
+    private static ProblemDetails ResourceNotFoundProblem => new()
+    {
+        Title = "Resource wasn't found.", Detail = "Requested resource or one of its dependencies wasn't found."
+    };
+
+    private static ProblemDetails RequestFailedProblem => new()
+    {
+        Title = "Request has failed.", Detail = "Unexpected internal error has occurred."
+    };
 }
--- src/Scheduler/Scheduler.Api/Startup.cs
+++ src/Scheduler/Scheduler.Api/Startup.cs
@@ -4,6 +4,7 @@ using Assistant.Net.Messaging.Options;
 using Assistant.Net.Scheduler.Api.Conventions;
 using Assistant.Net.Scheduler.Api.Handlers;
 using Assistant.Net.Scheduler.Api.Middlewares;
+using Assistant.Net.Scheduler.Contracts;
 using Assistant.Net.Scheduler.Contracts.Events;
 using Assistant.Net.Scheduler.Contracts.Models;
 using Assistant.Net.Serialization.Converters;
@@ -12,121 +13,124 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using System;
 using System.IO;
 using System.Text.Json.Serialization;
+using MongoNames = Assistant.Net.Scheduler.Contracts.MongoNames;
 
-namespace Assistant.Net.Scheduler.Api
+namespace Assistant.Net.Scheduler.Api;
+
+/// <summary>
+///     Scheduler API startup configuration.
+/// </summary>
+public class Startup
 {
-    /// <summary>
-    ///     Scheduler API startup configuration.
-    /// </summary>
-    public class Startup
+    /// <summary />
+    public Startup(IConfiguration configuration) =>
+        Configuration = configuration;
+
+    /// <summary />
+    public IConfiguration Configuration { get; }
+
+    /// <summary />
+    public void ConfigureServices(IServiceCollection services)
     {
-        /// <summary/>
-        public Startup(IConfiguration configuration) =>
-            Configuration = configuration;
+        services
+            .AddStorage(b => b
+                .UseMongo(Configuration.GetConnectionString(ConfigurationNames.Database))
+                .AddMongo<Guid, AutomationModel>()
+                .AddMongo<Guid, JobModel>()
+                .AddMongo<Guid, RunModel>()
+                .AddMongo<Guid, TriggerModel>())
+            .AddMongoMessageHandling(b => b
+                .UseMongo(ConfigureMongo)
+                .RemoveInterceptor<CachingInterceptor>()
+                .AddHandler<AutomationHandlers>()
+                .AddHandler<JobHandlers>()
+                .AddHandler<RunHandlers>()
+                .AddHandler<TriggerHandlers>())
+            .AddMessagingClient(b => b
+                .RemoveInterceptor<CachingInterceptor>()
+                .AddHandler<AutomationHandlers>()
+                .AddHandler<JobHandlers>()
+                .AddHandler<RunHandlers>()
+                .AddHandler<TriggerHandlers>()
+                .UseMongo(ConfigureMongo)
+                .AddMongo<RunSucceededEvent>()
+                .AddMongo<RunFailedEvent>());
 
-        /// <summary/>
-        public IConfiguration Configuration { get; }
+        // todo: configure logging, enrich request details, correlation id, machine name, thread...
+        //services.AddLogging(b => b.AddConsole());
 
-        /// <summary/>
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services
-                .AddStorage(b => b
-                    .UseMongo(Configuration.GetConnectionString(Contracts.ConfigurationNames.Database))
-                    .AddMongo<Guid, AutomationModel>()
-                    .AddMongo<Guid, JobModel>()
-                    .AddMongo<Guid, RunModel>()
-                    .AddMongo<Guid, TriggerModel>())
-                .AddMongoMessageHandling(b => b
-                    .UseMongo(ConfigureMongo)
-                    .RemoveInterceptor<CachingInterceptor>()
-                    .AddHandler<AutomationHandlers>()
-                    .AddHandler<JobHandlers>()
-                    .AddHandler<RunHandlers>()
-                    .AddHandler<TriggerHandlers>())
-                .AddMessagingClient(b => b
-                    .RemoveInterceptor<CachingInterceptor>()
-                    .AddHandler<AutomationHandlers>()
-                    .AddHandler<JobHandlers>()
-                    .AddHandler<RunHandlers>()
-                    .AddHandler<TriggerHandlers>()
-                    .UseMongo(ConfigureMongo)
-                    .AddMongo<RunSucceededEvent>()
-                    .AddMongo<RunFailedEvent>());
-
-            // todo: configure logging, enrich request details, correlation id, machine name, thread...
-            //services.AddLogging(b => b.AddConsole());
-
-            // todo: implement authorization
-            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme);
-                //.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, o => {});
-                //.AddOAuth(JwtBearerDefaults.AuthenticationScheme, o => {});
-            //services.AddAuthorization();
-            services
-                // todo: don't use serializer for api response serialization
-                //.AddSerializer(b => b.AddJsonType<ProblemDetails>())
-                .AddControllers(o =>
-                {
-                    o.Conventions.Add(new ResponseConvention());
-                    o.Conventions.Add(new ContentTypeConvention());
-                    // todo: implement https://restfulapi.net/hateoas/
-                    //options.Conventions.Add(new HateoasConvention());
-                });
-            services.Configure<JsonOptions, IServiceProvider>((o, p) =>
+        // todo: implement authorization
+        //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme);
+        //.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, o => {});
+        //.AddOAuth(JwtBearerDefaults.AuthenticationScheme, o => {});
+        //services.AddAuthorization();
+        services
+            .AddControllers(o =>
             {
-                o.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-                o.JsonSerializerOptions.Converters.Add(new AdvancedJsonConverterFactory(p));
-            });
-
-            var location = typeof(Startup).Assembly.Location;
-            var folderPath = Path.GetDirectoryName(location)!;
-            var fileName = Path.GetFileNameWithoutExtension(location);
-            services.AddSwaggerGen(o =>
-            {
-                o.IncludeXmlComments(Path.Combine(folderPath, fileName + ".xml"), includeControllerXmlComments: true);
+                o.Conventions.Add(new ResponseConvention());
+                o.Conventions.Add(new ContentTypeConvention());
                 // todo: implement https://restfulapi.net/hateoas/
-                //o.DocumentFilter<HateoasDocumentFilter>();
-                // todo: implement authorization for swagger
-                //o.AddSecurityDefinition("OAuth2", new OpenApiSecurityScheme { });
-                //o.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme { });
-                //o.AddSecurityRequirement(new OpenApiSecurityRequirement { });
+                //options.Conventions.Add(new HateoasConvention());
             });
-
-            services.AddTransient<CorrelationMiddleware>();
-            services.AddTransient<LoggingMiddleware>();
-            services.AddTransient<DiagnosticMiddleware>();
-            services.AddTransient<ErrorHandlingMiddleware>();
-        }
-
-        /// <summary/>
-        public void Configure(IApplicationBuilder app)
+        services.Configure<JsonOptions, IServiceProvider>((o, p) =>
         {
-            const string baseUrl = "api";
-            app
-                .UsePathBase($"/{baseUrl}")
-                .UseRouting()
-                // todo: implement authorization
-                //.UseAuthentication()
-                //.UseAuthorization()
-                .UseMiddleware<CorrelationMiddleware>()
-                .UseMiddleware<LoggingMiddleware>()
-                .UseMiddleware<ErrorHandlingMiddleware>()
-                .UseEndpoints(endpoints =>
-                {
-                    endpoints.MapControllers();
-                    // todo: implement authorization
-                    //.RequireAuthorization()
-                })
-                .UseSwagger(o => o.PreSerializeFilters.Add((doc, req) => doc.Servers.Add(new() {Url = $"{req.Scheme}://{req.Host}/{baseUrl}"})))
-                .UseSwaggerUI()
-                .UseMiddleware<DiagnosticMiddleware>();
-        }
+            o.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            o.JsonSerializerOptions.Converters.Add(new AdvancedJsonConverterFactory(p));
+        });
 
-        private void ConfigureMongo(MongoOptions options) => options
-            .Connection(Configuration.GetConnectionString(Contracts.ConfigurationNames.Messaging))
-            .Database(Contracts.MongoNames.DatabaseName);
+        var location = typeof(Startup).Assembly.Location;
+        var folderPath = Path.GetDirectoryName(location)!;
+        var fileName = Path.GetFileNameWithoutExtension(location);
+        services.AddSwaggerGen(o =>
+        {
+            o.IncludeXmlComments(Path.Combine(folderPath, fileName + ".xml"), true);
+            // todo: implement https://restfulapi.net/hateoas/
+            //o.DocumentFilter<HateoasDocumentFilter>();
+            // todo: implement authorization for swagger
+            //o.AddSecurityDefinition("OAuth2", new OpenApiSecurityScheme { });
+            //o.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme { });
+            //o.AddSecurityRequirement(new OpenApiSecurityRequirement { });
+        });
+
+        services.AddTransient<CorrelationMiddleware>();
+        services.AddTransient<LoggingMiddleware>();
+        services.AddTransient<DiagnosticMiddleware>();
+        services.AddTransient<ErrorHandlingMiddleware>();
+    }
+
+    /// <summary />
+    public void Configure(IApplicationBuilder app)
+    {
+        const string baseUrl = "api";
+        app
+            .UsePathBase($"/{baseUrl}")
+            .UseRouting()
+            // todo: implement authorization
+            //.UseAuthentication()
+            //.UseAuthorization()
+            .UseMiddleware<CorrelationMiddleware>()
+            .UseMiddleware<LoggingMiddleware>()
+            .UseMiddleware<ErrorHandlingMiddleware>()
+            .UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                // todo: implement authorization
+                //.RequireAuthorization()
+            })
+            .UseSwagger(o =>
+                o.PreSerializeFilters.Add((doc, req) => doc.Servers.Add(new OpenApiServer {Url = $"{req.Scheme}://{req.Host}/{baseUrl}"})))
+            .UseSwaggerUI()
+            .UseMiddleware<DiagnosticMiddleware>();
+    }
+
+    private void ConfigureMongo(MongoOptions options)
+    {
+        options
+            .Connection(Configuration.GetConnectionString(ConfigurationNames.Messaging))
+            .Database(MongoNames.DatabaseName);
     }
 }
